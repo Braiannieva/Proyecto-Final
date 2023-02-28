94e083da3b9e3af9c466fa70ccb8f77f3430d023
Braiannieva <104873531+Braiannieva@users.noreply.github.com>
2023-02-28 12:09:47 -0300

    Add files via upload

---
+++ curriculum.js
@@ -0,0 +1,8 @@
+function function1() {
+    document.getElementById("uno").style.display = "block"
+    document.getElementById("dos").style.display = "none"
+}
+function function2() {
+    document.getElementById("dos").style.display = "block"
+    document.getElementById("uno").style.display = "none"
+}
